abc5d09d73d7beb1a91e4ad90e04a61e7bdeacbc
Dalmo Felipe <dalmo.felipe@gmail.com>
2023-08-06 08:34:20 -0300

    init

---
+++ src/main/java/com/dalmofelipe/springtemplate/SpringTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.dalmofelipe.springtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dalmofelipe/springtemplate/SpringTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dalmofelipe.springtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
