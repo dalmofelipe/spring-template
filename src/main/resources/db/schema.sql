7e4a29b3355822f7358b43da3013ca9eb96ccf1a
Dalmo Felipe <dalmo.felipe@gmail.com>
2023-09-01 17:17:39 -0300

    add schema sql

---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS tb_users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
