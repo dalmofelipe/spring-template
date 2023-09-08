9ece67149652e42c6cec7d153c60f382796f3964
Dalmo Felipe <dalmo.felipe@gmail.com>
2023-09-08 13:55:27 -0300

    implements flyway migration and production database with postgres

---
+++ prod.env.sh
@@ -0,0 +1,8 @@
+## Excute o script com source ou . para adicionar as variáveis no sistema
+## . prod.sh
+#!/bin/bash
+
+export PG_DATABASE=spring
+export PG_USER=myuser
+export PG_PASSWORD=123123
+# export PG_SCHEMA=template
---
+++ src/main/resources/db/migration/V1_1_1__update_user_schema.sql
@@ -0,0 +1,15 @@
+-- Alteração necessária para corrigir o bind de UUID para string ao recuperar dados do usuário no Postgres
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+
+ALTER TABLE TB_USERS ALTER COLUMN id SET DATA TYPE UUID USING (uuid_generate_v4());
+
+
+INSERT INTO TB_USERS(id, name, email, password, role)
+VALUES 
+('df6e663a-f302-444a-a7fa-5ebecfa160de', 'Dalmo', 'dalmo@email.com', '123123', 'JEDI'),
+('07300028-a924-47cf-ad54-1c147a95a9a4', 'Felipe', 'felipe@email.com', '321321', 'JUNIOR'),
+('94f182df-6cad-405d-85bb-6851bfa1effc' ,'Torres', 'torres@email.com', '123456', 'PLENO'),
+('5f08a94e-3a9a-4daa-b4af-07af03b4523a', 'Paula', 'Paula@email.com', '656565', 'PLENO'),
+('11433ddc-efa8-4e62-b7fe-8bdc3ba46bb6', 'De', 'de@email.com', '006007', 'JUNIOR');
